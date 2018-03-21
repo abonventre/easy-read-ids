b17ca02366604a0c51e7e7258a5b870e894d6fff
Anthony Bonventre <abonventre@gmail.com>
2018-03-21 14:41:43 -0400

    Added tons of items and function to check id space of current config

--- index.js
+++ index.js
@@ -1,9 +1,117 @@
-exports.adjectives = ['fun', 'adorable', 'sparkling', 'clever', 'smart'];
-exports.animals = ['kangaroo', 'dog', 'cat', 'fish', 'octopus', 'elephant'];
-exports.nautical = ['line', 'anchor', 'propeller', 'hull', 'rudder'];
-exports.colors = ['red', 'green', 'blue', 'purple', 'pink', 'orange', 'gray', 'yellow'];
-exports.nouns = ['car', 'shoe', 'mountain', 'river', 'ocean', 'building', 'house', 'flowers', 'clock'];
-exports.verbs = ['flying', 'jumping', 'swimming', 'driving', 'playing', 'sleeping'];
+
+// 190
+exports.adjective = ['able', 'adorable', 'academic', 'accurate', 'acrobatic',
+  'active', 'admired', 'advanced', 'afraid', 'agile', 'alarmed', 'alert',
+  'amazing', 'ambitious', 'amused', 'another', 'anxious', 'arctic', 'artistic',
+  'athletic', 'attractive', 'authentic', 'average', 'aware', 'awesome', 'awkward',
+  'basic', 'beautiful', 'better', 'best', 'bold', 'bouncy', 'brave', 'bright',
+  'brilliant', 'busy', 'buzzing', 'calm', 'careful', 'caring', 'cautious',
+  'charming', 'cheerful', 'classic', 'clean', 'clever', 'cold', 'colorful', 'cool',
+  'crafty', 'creative', 'cute', 'dangerous', 'dark', 'different', 'digital',
+  'dirty', 'dizzy', 'dramatic', 'early', 'electric', 'energetic', 'exhausted',
+  'exotic', 'expert', 'fabulous', 'fair', 'fake', 'fancy', 'fantastic', 'fast',
+  'fluffy', 'focused', 'formal', 'free', 'frosty', 'frozen', 'full', 'funny',
+  'fuzzy', 'gently', 'gifted', 'glossy', 'golden', 'good', 'great', 'grown',
+  'handy', 'happy', 'harmless', 'healthy', 'helpful', 'hidden', 'honest', 'honorable',
+  'hopeful', 'hot', 'humble', 'hungry', 'ideal', 'imaginary', 'infamous', 'jolly',
+  'joyful', 'junior', 'jumpy', 'kind', 'knowing', 'last', 'little', 'lost',
+  'loud', 'loving', 'loyal', 'lucky', 'mad', 'mean', 'mellow', 'merry', 'messy',
+  'modern', 'muddy', 'neat', 'nervous', 'new', 'nice', 'nimble', 'noisy',
+  'odd', 'outgoing', 'peaceful', 'perfect', 'pleasant', 'popular', 'powerful',
+  'practical', 'pretty', 'proper', 'qualified', 'quick', 'quiet', 'quirky',
+  'radiant', 'rare', 'real', 'reliable', 'respectful', 'rich', 'robust', 'royal',
+  'rusty', 'safe', 'sandy', 'scared', 'silent', 'silly', 'simple', 'sleepy',
+  'small', 'smart', 'snappy', 'soft', 'spotted', 'strong', 'stunning', 'sturdy',
+  'super', 'sweet', 'tall', 'tame', 'thirsty', 'thrifty', 'tidy', 'tiny', 'tough',
+  'truthful', 'ultimate', 'unlucky', 'upbeat', 'useful', 'vibrant', 'vivid', 'warm',
+  'wavy', 'wild', 'wise', 'winged', 'wonderful', 'worried', 'young'
+];
+
+// 133
+exports.animal = ['ant', 'bat', 'badger', 'bear', 'bee', 'bird', 'buffalo', 'cat', 'chicken',
+  'cow', 'crow', 'dog', 'dolphin', 'dove', 'duck', 'elephant', 'fish', 'fox', 'frog', 'goose',
+  'goat', 'horse', 'kangaroo', 'lion', 'monkey', 'owl', 'ox', 'parrot', 'pigeon', 'penguin', 'pig',
+  'rabbit', 'sheep', 'tiger', 'whale', 'wolf', 'zebra', 'swan', 'pelican', 'bobcat', 'snake', 'cobra',
+  'flamingo', 'chipmunk', 'deer', 'dingo', 'eagle', 'elk', 'falcon', 'gecko', 'otter',
+  'bison', 'coyote', 'beetle', 'butterfly', 'crab', 'lobster', 'flounder', 'gorilla', 'shark',
+  'seal', 'jaguar', 'lizard', 'mole', 'mongoose', 'moose', 'newt', 'octopus', 'oyster', 'panther',
+  'possum', 'mouse', 'puma', 'raccoon', 'robin', 'scorpion', 'seahorse', 'skunk',
+  'sparrow', 'toucan', 'turkey', 'squid', 'starfish', 'walrus', 'wasp', 'ladybug', 'python',
+  'rattlesnake', 'dragonfly', 'hen', 'stork', 'sloth', 'vulture', 'boar', 'woodchuck', 'alligator',
+  'dinosaur', 'armadillo', 'baboon', 'bass', 'camel', 'catfish', 'cheetah', 'clam', 'crocodile',
+  'donkey', 'dragon', 'earthworm', 'eel', 'fly', 'giraffe', 'gopher', 'hamster', 'hornet',
+  'jellyfish', 'snail', 'lemur', 'leopard', 'moth', 'mule', 'peacock', 'pony', 'puffin',
+  'raven', 'rooster', 'salmon', 'shrimp', 'toad', 'turtle', 'tuna', 'unicorn', 'viper', 'wolverine'
+];
+
+// 128
+exports.nautical = ['line', 'anchor', 'propeller', 'hull', 'rudder', 'sailboat',
+  'bell', 'compass', 'port', 'starboard', 'aft', 'forward', 'ship', 'admiral',
+  'captain', 'deckhand', 'ahoy', 'barge', 'beam', 'mast', 'bearing', 'below',
+  'boat', 'sand', 'boom', 'bow', 'bridge', 'buoy', 'cabin', 'cable', 'cardinal',
+  'north', 'south', 'west', 'east', 'chain', 'cleat', 'clipper', 'cog', 'convoy',
+  'crab', 'clam', 'cruise', 'cutwater', 'dart', 'deck', 'dock', 'drink',
+  'diver', 'drydock', 'fairwater', 'fast', 'slow', 'fender', 'fin', 'keel', 'flag',
+  'signal', 'sos', 'flank', 'foot', 'force', 'fore', 'galley', 'gangplank', 'ghost',
+  'gooseneck', 'harbor', 'hatch', 'header', 'helm', 'hitch', 'horn', 'hounds',
+  'icing', 'icebreaker', 'inboard', 'island', 'jetty', 'jib', 'knee', 'knock',
+  'lace', 'ladder', 'lanyard', 'launch', 'lookout', 'marina', 'mess', 'mile',
+  'mooring', 'chart', 'navigation', 'ocean', 'offshore', 'outdrive', 'overboard',
+  'pier', 'pilot', 'pirate', 'wheel', 'radar', 'ram', 'reef', 'rigging', 'roll',
+  'rope', 'sail', 'shipyard', 'skipper', 'slip', 'sonar', 'spar', 'stern', 'sweep',
+  'tackle', 'tail', 'tender', 'officer', 'topmast', 'trim', 'vessel', 'voyage',
+  'watch', 'waterway', 'wheelhouse', 'winch', 'wind'
+];
+
+// 44
+exports.color = ['amber', 'apricot', 'aqua', 'black', 'blue', 'bronze', 'brown',
+  'cardinal', 'charcoal', 'chocolate', 'copper', 'coral', 'cream', 'crimson',
+  'eggplant', 'emerald', 'green', 'gold', 'indigo', 'jade', 'lemon', 'lime',
+  'magenta', 'maroon', 'mustard', 'olive', 'orange', 'peach', 'pink', 'purple',
+  'rainbow', 'red', 'rose', 'ruby', 'rust', 'silver', 'slate', 'steel', 'tan',
+  'teal', 'tomato', 'violet', 'white', 'yellow'
+];
+
+// 190
+exports.noun = ['apple', 'bag', 'balloon', 'bananas', 'bed',
+  'book', 'bookmark', 'bottle', 'bowl', 'box', 'bracelet',
+  'bread', 'brocolli', 'bucket', 'button', 'camera', 'candle', 'candy',
+  'canvas', 'car', 'greeting', 'card', 'carrot', 'cat', 'phone',
+  'peanut', 'block', 'chair', 'chalk', 'newspaper', 'checkbook', 'chocolate',
+  'clay', 'clock', 'clothes', 'computer',
+  'conditioner', 'cookie', 'cork', 'couch', 'cup', 'desk',
+  'door', 'drawer', 'drill', 'eraser', 'eye', 'face',
+  'flag', 'floor', 'flowers', 'food', 'fork', 'fridge', 'glass', 'glasses', 'stick',
+  'hair', 'hanger', 'helmet', 'house', 'charger', 'key',
+  'keyboard', 'knife', 'lamp', 'shade', 'milk', 'mirror', 'model car', 'money',
+  'monitor', 'mop', 'nail', 'file', 'needle', 'outlet', 'paint', 'pants', 'paper',
+  'pen', 'pencil', 'perfume', 'photo', 'picture', 'pillow', 'plastic',
+  'plate', 'pool', 'soda', 'puddle', 'purse', 'blanket', 'radio', 'remote', 'ring',
+  'rubber', 'rug', 'sailboat', 'sand', 'sandal', 'tape', 'screw', 'seat', 'shampoo',
+  'sharpie', 'shirt', 'shoe', 'shovel', 'sidewalk', 'slipper', 'soap', 'socks', 'sofa', 'speakers',
+  'sponge', 'spoon', 'spring', 'note', 'stockings', 'sign', 'street',
+  'sunglasses', 'table', 'television', 'thermometer', 'tire',
+  'tissue', 'tomato', 'toothbrush', 'toothpaste', 'towel', 'tree', 'truck', 'tv',
+  'twezzers', 'twister', 'vase', 'video', 'wallet',
+  'machine', 'watch', 'water', 'doll', 'magnet', 'wagon', 'headphones',
+  'clamp', 'air', 'piano', 'ice', 'window',
+  'controller', 'coasters', 'thermostat', 'zipper', 'university', 'teacher',
+  'disk', 'technology', 'soup', 'apartment', 'energy', 'lake', 'office', 'carpet',
+  'baseball', 'basketball', 'paperclip', 'postcard', 'coin', 'brush', 'pen',
+  'bank', 'laptop', 'battery', 'magazine', 'alarm', 'plane', 'dress', 'mailbox',
+  'lunch', 'wire', 'jar', 'eggplant', 'microphone', 'taco', 'star', 'necklace',
+  'mouse'
+];
+
+//47
+exports.action = ['flying', 'jumping', 'swimming', 'driving', 'playing', 'sleeping',
+  'napping', 'screaming', 'teaching', 'laughing', 'painting', 'reading', 'joking',
+  'singing', 'running', 'walking', 'listening', 'jogging', 'biking', 'skating',
+  'dancing', 'sneezing', 'coughing', 'skipping', 'shopping', 'drawing', 'cleaning',
+  'hopping', 'crawling', 'climbing', 'talking', 'working', 'waving', 'blinking',
+  'hiding', 'dreaming', 'sailing', 'planting', 'gardening', 'building', 'standing',
+  'bouncing', 'floating', 'juggling', 'marching', 'knocking', 'spinning'
+];
 
 function rnd(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -13,17 +121,19 @@ function rndArray(array) {
   return array[rnd(array.length)];
 }
 
-exports.generate = function(list, maxRandom) {
+// Generate an easy-read-id using an optional list, seperator and max number
+exports.generate = function(list, maxRandom, seperator) {
   let componentsList = list || ['adjective', 'animal', 'number'];
   let max = maxRandom || 100;
+  let joinSeperator = seperator || '-';
   let readableID = [];
   let elements = {
-    adjective: rndArray(exports.adjectives),
-    animal: rndArray(exports.animals),
+    adjective: rndArray(exports.adjective),
+    animal: rndArray(exports.animal),
     nautical: rndArray(exports.nautical),
-    color: rndArray(exports.colors),
-    noun: rndArray(exports.nouns),
-    verb: rndArray(exports.verbs),
+    color: rndArray(exports.color),
+    noun: rndArray(exports.noun),
+    action: rndArray(exports.action),
     number: rnd(max)
   }
 
@@ -31,9 +141,28 @@ exports.generate = function(list, maxRandom) {
     readableID.push(elements[componentsList[i]]);
   }
 
-  return readableID.join("-");
+  return readableID.join(joinSeperator);
+}
+
+
+// Calculate id space available
+exports.getIDSpace = function(list, maxRandom) {
+  let componentsList = list || ['adjective', 'animal', 'number'];
+  let max = maxRandom || 100;
+  let space = 1;
+
+  for (let j = 0; j < componentsList.length; j++) {
+    if(componentsList[j] === 'number') {
+      space = space * max;
+    } else {
+      space = exports[componentsList[j]].length * space;
+    }
+  }
+  return space;
 }
 
 for (var i = 0; i < 20; i++) {
-  console.log(exports.generate());
+  console.log(exports.generate(['color', 'action', 'adjective', 'animal', 'number'], 100, ' | '));
 }
+
+console.log(exports.getIDSpace());
--- readme.md
+++ readme.md
@@ -1,2 +1,22 @@
 # Easy Read Ids
-Easy to read and write IDs for node applications.
+Easy to read and write IDs for javascript applications.
+
+Generates `mostly` unique IDs that should be easy to read and write.  ID's are
+generated using a simple makeup of any of the following `or all of them`
+
+* Actions
+* Animals
+* Colors
+* Adjectives
+* Numbers
+* Nautical Items
+
+*That last one is a strange one, I know... I work for a ferry company :-)*
+
+### Installation
+
+```sh
+$ npm install easy-read-ids
+```
+
+### Installation
